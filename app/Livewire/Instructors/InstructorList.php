b145820d43309e53896bb4634db3b96882d1125b
Najeeb Anwari <najeeb.anwari@gmail.com>
2023-12-16 10:25:07 +0430

    Created InstructorList page

---
+++ app/Livewire/Instructors/InstructorList.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Instructors;
+
+use Livewire\Component;
+
+class InstructorList extends Component
+{
+    public function render()
+    {
+        return view('livewire.instructors.instructor-list');
+    }
+}
