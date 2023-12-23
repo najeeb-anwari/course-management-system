7aedd29516c7e84f460966178c6d4f87c4a3487d
Najeeb Anwari <najeeb.anwari@gmail.com>
2023-12-23 21:54:56 +0430

    Created page components for students

---
+++ app/Livewire/Students/StudentCreate.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Students;
+
+use Livewire\Component;
+
+class StudentCreate extends Component
+{
+    public function render()
+    {
+        return view('livewire.students.student-create');
+    }
+}
---
+++ app/Livewire/Students/StudentEdit.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Students;
+
+use Livewire\Component;
+
+class StudentEdit extends Component
+{
+    public function render()
+    {
+        return view('livewire.students.student-edit');
+    }
+}
