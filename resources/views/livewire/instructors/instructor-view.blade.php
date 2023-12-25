2eacdc0cdb3217eee946804f0f5897b617d2f165
Najeeb Anwari <najeeb.anwari@gmail.com>
2023-12-26 00:08:43 +0430

    Added __() functions for localization of words

--- resources/views/livewire/instructors/instructor-view.blade.php
+++ resources/views/livewire/instructors/instructor-view.blade.php
@@ -7,13 +7,14 @@
                 <div class="border-bottom pb-3 mb-3 d-flex justify-content-between align-items-center">
                     <div class="mb-2 mb-lg-0">
                         <h1 class="mb-1 h2 fw-bold">
-                            View Instructor
+                            {{ __('View Instructor') }}
                         </h1>
                         <!-- Breadcrumb  -->
                         <livewire:breadcrumb :items="$breadcrumbItems" />
                     </div>
                     <div>
-                        <a wire:navigate href="{{ route('instructors.index') }}" class="btn btn-primary">{{ __("Back to List") }}</a>
+                        <a wire:navigate href="{{ route('instructors.index') }}"
+                            class="btn btn-primary">{{ __('Back to List') }}</a>
                     </div>
                 </div>
             </div>
@@ -25,18 +26,19 @@
                     <div class="row ">
                         <div class="col-md-4">
                             @if ($instructor->photo_url)
-                                <img class="rounded-circle" style="max-width: 150px" src="{{ asset('storage' . $instructor->photo_url) }}">
+                                <img class="rounded-circle" style="max-width: 150px"
+                                    src="{{ asset('storage' . $instructor->photo_url) }}">
                             @else
-                                <p>No photo available</p>
+                                <p>{{ __('No photo available') }}</p>
                             @endif
                         </div>
                         <div class="col-md-8">
                             <h1>{{ $instructor->name }}</h1>
-                            <p>Father's Name: {{ $instructor->father_name }}</p>
-                            <p>Position: {{ $instructor->position }}</p>
-                            <p>Phone Number: {{ $instructor->phone_number }}</p>
-                            <p>Email: {{ $instructor->email }}</p>
-                            <p>Joined at: {{ $instructor->created_at->diffForHumans() }}</p>
+                            <p>{{ __("Father's Name") }}: {{ $instructor->father_name }}</p>
+                            <p>{{ __('Position') }}: {{ $instructor->position }}</p>
+                            <p>{{ __('Phone Number') }}: {{ $instructor->phone_number }}</p>
+                            <p>{{ __('Email') }}: {{ $instructor->email }}</p>
+                            <p>{{ __('Joined at') }}: {{ $instructor->created_at->diffForHumans() }}</p>
                         </div>
                     </div>
                 </div>
