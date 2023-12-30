e4585ee01dca1673a8491983e3bd52ac0c7f1965
Najeeb Anwari <najeeb.anwari@gmail.com>
2023-12-31 00:34:44 +0430

    added relationships for category and parent category and updated filllable

--- app/Models/CourseCategory.php
+++ app/Models/CourseCategory.php
@@ -8,4 +8,36 @@ use Illuminate\Database\Eloquent\Model;
 class CourseCategory extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'category_name',
+        'slug',
+        'description',
+        'parent_category_id',
+    ];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($category) {
+            if (!$category->parent_category_id) {
+                $category->parent_category_id = null;
+            }
+        });
+    }
+
+    public function parentCategory()
+    {
+        return $this->belongsTo(CourseCategory::class, 'parent_category_id');
+    }
+
+    public function childCategories()
+    {
+        return $this->hasMany(CourseCategory::class, 'parent_category_id');
+    }
+
+    protected static function newFactory()
+    {
+        return \Database\Factories\CourseCategoryFactory::new();
+    }
 }
